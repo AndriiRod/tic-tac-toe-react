2791b547921a86ae93ea7762aa349ae34805c13e
Andrii <rod.andriiw@gmail.com>
2024-01-16 20:12:50 +0000

    add gameboard logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handlerSelectSquare(rowIndex, colIndex) {
+    setGameBoard(prevState => {
+      const updateBoard = [...prevState.map(innerArray => [...innerArray])];
+      updateBoard[rowIndex][colIndex] = 'X';
+      return updateBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((symbol, rowIndex) => (
-              <li key={rowIndex}>
-                <button>{symbol}</button>
+            {row.map((symbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handlerSelectSquare(rowIndex, colIndex)}>
+                  {symbol}
+                </button>
               </li>
             ))}
           </ol>
